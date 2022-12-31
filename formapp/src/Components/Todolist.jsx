8e948b799620e6f62db73e60d6994058ebad094e
rafik  shaikh <rafikshaikhbiloli123@gmail.com>
2022-12-31 18:56:28 +0530

    add

--- formapp/src/Components/Todolist.jsx
+++ formapp/src/Components/Todolist.jsx
@@ -8,14 +8,25 @@ const Todolist = () => {
     const handleClick =() => {
         setData([...data,text])
     }
+    const handleDelet = (item) => {
+        const filtered = data.filter((e)=> e!== item )
+        setData([...filtered])
+    }
 
     return (
         <div>
             <h1>This is Todolist</h1>
             <input type="text"  placeholder="fill text"  onChange={(e)=> setText(e.target.value)} />
             <button onClick={handleClick} >Add</button>
+             {data.map((item)=>{
+                return(
+                    <div>
+                        <h1 >{item}</h1>
+                        <button onClick={()=> handleDelet(item)}>delet</button>
+                    </div>
+                )
+             })}
 
-            
             
         </div>
     );
